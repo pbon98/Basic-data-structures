d4b92e77179c5bb967408b86c08a47e16dadacf5
pbon98 <pbaughan15@gmail.com>
2023-03-15 20:45:04 -0400

    basic-data-structures free-code-camp

---
+++ step12.js
@@ -0,0 +1,13 @@
+let myNestedArray = [
+  // Only change code below this line
+  'level 1',
+  ['level 2'],
+  [['level 3', 'deep']],
+  [[['level 4', 'deeper']]],
+  [[[['level 5', 'deepest']]]]
+
+
+  // Only change code above this line
+];
+
+console.log(myNestedArray)
